182507940979d0ce64f9caa4b12d5d45d8ca7373
Miguel Pérez <epsilonexe2@gmail.com>
2025-04-15 23:35:12 -0400

    feat: cache historial

--- 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { GifService } from '../../../services/gifs.service';
 
 interface MenuOption {
   icon: string;
@@ -14,6 +15,13 @@ interface MenuOption {
   templateUrl: './side-menu-options.component.html',
 })
 export class SideMenuOptionsComponent {
+  constructor(){
+    this.loadHistory()
+  }
+
+  gifService = inject(GifService)
+  keys = signal<string[]>([])
+
   menuOptions: MenuOption[] = [
     {
       icon: 'fa-solid fa-chart-line',
@@ -28,4 +36,11 @@ export class SideMenuOptionsComponent {
       route: '/dashboard/search',
     },
   ];
+  
+  loadHistory(){
+    this.keys.set(Object.keys(this.gifService.searchHistoryKeys()));
+    console.log(this.keys())
+    
+  }
+
 }
--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,35 +1,65 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
-import { environment } from '@environments/environment';
-import { GiphyResponse } from '../interfaces/giphy.interfaces';
-import { Gif } from '../interfaces/gif.interface';
-import { GifMapper } from '../mapper/gif.mapper';
+import { HttpClient } from "@angular/common/http";
+import { computed, inject, Injectable, signal } from "@angular/core";
+import { environment } from "@environments/environment";
+import { GiphyResponse } from "../interfaces/giphy.interfaces";
+import { Gif } from "../interfaces/gif.interface";
+import { GifMapper } from "../mapper/gif.mapper";
+import { map, Observable, tap } from "rxjs";
 
 
-@Injectable({providedIn: 'root'})
+
+
+
+@Injectable({ providedIn: "root" })
 export class GifService {
-    
-    private http = inject(HttpClient);
+  private http = inject(HttpClient);
 
-    trengingGifs = signal<Gif[]>([]);
-    
+  trendingGifs = signal<Gif[]>([]);
+  trendingGIfsLoading = signal(true);
+  searchingGifs = signal<Gif[]>([]);
+  searchHistory = signal<Record<string, Gif[]>>({});
+  searchHistoryKeys = computed(() => {
+    return Object.keys(this.searchHistory());
+  });
 
-    constructor(){
-        this.loadTrendingGifs();
-    }
+  constructor() {
+    this.loadTrendingGifs();
+  }
 
-    loadTrendingGifs(){
-        this.http.get<GiphyResponse>(`${ environment.giphyUrl }/gifs/trending`,{
-            params: {
-                api_key: environment.giphyApiKey,
-                limit: 20
-            }
-        }).subscribe( (resp) => {
-            const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
-            console.log({ gifs })
-            
-        } );
-    }
+  loadTrendingGifs() {
+    this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 20,
+        },
+      })
+      .subscribe((resp) => {
+        const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+        this.trendingGifs.set(gifs);
+        this.trendingGIfsLoading.set(false);
+      });
+  }
 
-    
+  searchGifs(query: string) {
+    return this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 25,
+          q: query,
+        },
+      })
+      .pipe(
+        map(({ data }) => data),
+        map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
+
+        //historial
+
+        tap( items => this.searchHistory.update((history) => ({
+            ...history,
+            [query.toLowerCase()]: items
+        })))
+      );
+  }
 }
